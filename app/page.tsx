3edfc7b3a7216d4c84d0c40ef41a9ade298521c5
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-08-25 18:11:50 +0300

    add the template for the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className='flex w-full grow justify-between items-center'>
+      <div>
+        <div className='w-1/2'>
+          <h1 className='mainHeading'>Hi! I am <strong className='font-bold'>Ivan Danyliuk</strong>.</h1>
+          <h2 className='subHeading'>I am a creative frontend developer based in Ukraine</h2>
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <div>
+        
+      </div>
     </div>
   )
 }
