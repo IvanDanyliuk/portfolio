7771536b446a12a6366591d9c0237770b51afbf7
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-09-07 20:26:30 +0300

    removed unused variables and imports

--- app/admin/biography/page.tsx
+++ app/admin/biography/page.tsx
@@ -5,32 +5,12 @@ import BiographyForm from '@/components/forms/BiographyForm'
 import SkillsList from '@/components/ui/SkillsList'
 import EducationList from '@/components/ui/EducationList'
 
-// const techSkills = [
-//   { id: '1', title: 'JavaScript' }, { id: '2', title: 'TypeScript' }, { id: '3', title: 'React' }, 
-//   { id: '4', title: 'Next.js' }, { id: '5', title: 'Redux/ReduxToolkit' }, { id: '6', title: 'HTML' }, 
-//   { id: '7', title: 'CSS/SASS/SCSS' }, { id: '8', title: 'Node.js' }, { id: '9', title: 'MongoDB' }, 
-//   { id: '10', title: 'MaterialUI' }, { id: '11', title: 'TailwindCSS' }
-// ]
-
-const education = [
-  {
-    period: '2021-2022',
-    place: 'SoftServe IT Academy',
-    degree: '',
-  },
-  {
-    period: '2008-2014',
-    place: 'Donetsk State University of Management',
-    degree: 'Master of Management',
-  }
-]
-
 const Biography: React.FC = async () => {
   const userData = await fetchUserData();
-  const user = JSON.parse(JSON.stringify(userData))
+  const user = JSON.parse(JSON.stringify(userData));
+  
   const techSkills = user.skills ? user.skills.filter((skill: any) => !skill.isAdditional) : [];
   const additionalSkills = user.skills ? user.skills.filter((skill: any) => skill.isAdditional) : [];
-
   
   return (
     <div>
