ce8ef1641a1c23452527530e26172bbdcb7772bf
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-09-18 11:55:35 +0300

    add useWindowSize custom hook

--- components/navigation/AdminNavbar.tsx
+++ components/navigation/AdminNavbar.tsx
@@ -4,16 +4,19 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 import { v4 as uuid } from 'uuid';
 import { adminPanelLinks } from '@/constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useWindowSize } from '@/hooks/useWindowSize';
 
 const AdminNavbar = () => {
   const pathname = usePathname();
+  const { width } = useWindowSize();
 
   return (
-    <ul className='w-full md:w-72 flex flex-row md:flex-col gap-3'>
-        {adminPanelLinks.map(({ label, link }) => (
-          <li key={uuid()}>
+    <ul className='relative w-full md:w-72 flex flex-row md:flex-col gap-3'>
+        {adminPanelLinks.map(({ label, icon, link }) => (
+          <li key={uuid()} className='w-1/4 md:w-auto text-center'>
             <Link href={link} className={pathname === link ? 'admin-nav-link__active' : 'admin-nav-link'}>
-              {label}
+              {width! > 786 ? label : (<FontAwesomeIcon icon={icon} />)}
             </Link>
           </li>
         ))}
--- constants/index.ts
+++ constants/index.ts
@@ -2,6 +2,7 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faBook, faCircleInfo, faFileContract, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 
 export const navLinks = [
   { label: 'HOME', link: '/' },
@@ -11,10 +12,10 @@ export const navLinks = [
 ];
 
 export const adminPanelLinks = [
-  { label: 'Summary', link: '/admin' },
-  { label: 'Biography & Skills', link: '/admin/biography' },
-  { label: 'Certifications', link: '/admin/certifications' },
-  { label: 'Projects', link: '/admin/projects' },
+  { label: 'Summary', icon: faCircleInfo, link: '/admin' },
+  { label: 'Biography & Skills', icon: faBook, link: '/admin/biography' },
+  { label: 'Certifications', icon: faFileContract, link: '/admin/certifications' },
+  { label: 'Projects', icon: faLaptopCode, link: '/admin/projects' },
   
 ]
 
