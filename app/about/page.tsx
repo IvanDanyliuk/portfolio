bb991973e05ed29b411c9e6de8a8f522f709c829
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-09-20 20:29:42 +0300

    add the mobile styles for the About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,7 @@ import userPhoto from '@/public/assets/photo.png'
 import { formatDate } from '@/lib/helpers/heplers'
 import { fetchCertifications } from '@/lib/actions/certifications.actions'
 import CertificateModal from '@/components/ui/CertificateModal'
+import { Skill } from '@/common.types'
 
 const About = async () => {
   const userInfo = await fetchUserData();
@@ -15,39 +16,41 @@ const About = async () => {
 
   return (
     <div>
-      <section className='pt-3 w-full flex gap-x-6 border-b-[1px] border-gray-100'>
-        <div className='flex border-r-[1px] border-gray-100'>
-          <div>
-            <h2 className='mb-7 text-4xl'>Hi! My name is <strong className='font-bold'>Ivan Danyliuk</strong></h2>
-            <div dangerouslySetInnerHTML={{ __html: userInfo.generalData.biography }} className='pr-7 font-base' />
-          </div>
-          <Image src={userPhoto} alt='photo' className='bottom-0 w-auto h-[450px] flex' />
+      <section className='relative pt-3 w-full flex flex-col md:flex-row gap-6 border-b-[1px] border-gray-100'>
+        <div className='w-full md:w-3/4'>
+          <h2 className='mb-7 text-4xl'>Hi! My name is <strong className='font-bold'>Ivan Danyliuk</strong></h2>
+          <div dangerouslySetInnerHTML={{ __html: userInfo.generalData.biography }} className='md:pr-7 text-sm md:text-base' />
         </div>
-        <SocialMediaList orientation='vertical' />
+        <div className='w-full md:w-1/4 h-full flex justify-around'>
+          <Image src={userPhoto} alt='photo' className='bottom-0 w-auto h-[450px] flex border-r border-gray-100' />
+          <SocialMediaList orientation='vertical' />
+        </div>
       </section>
-      <section className='py-14 border-b-[1px]'>
-        <h3 className='mb-6 text-4xl text-center font-semibold'>Experience</h3>
-        <ul>
-          {userInfo.experience.map(item => (
-            <li key={uuid()} className='flex gap-3 md:gap-16'>
-              <div className='text-base'>
-                {`${formatDate(item.periodFrom, 'month/year')} - ${formatDate(item.periodTo, 'month/year')}`}
-              </div>
-              <div>
-                <p className='text-lg font-semibold'>{item.position}</p>
-                <p className='font-semibold'>{item.company}</p>
-                <p className='mt-2 font-semibold italic'>Responsibilities:</p>
-                <div dangerouslySetInnerHTML={{ __html: item.responsibilities }} className='content-list' />
-              </div>
-            </li>
-          ))}
-        </ul>
+      <section className='py-10 md:py-14 border-b-[1px]'>
+        <h3 className='mb-6 text-2xl md:text-4xl text-center font-semibold'>Experience</h3>
+        <table>
+          <tbody>
+            {userInfo.experience.map(item => (
+              <tr key={uuid()}>
+                <td className='text-xs md:text-base h-full pr-2 md:pr-6 align-top'>
+                  {`${formatDate(item.periodFrom, 'month/year')}-${formatDate(item.periodTo, 'month/year')}`}
+                </td>
+                <td>
+                  <p className='text-sm md:text-lg font-semibold'>{item.position}</p>
+                  <p className='text-xs md:text-base font-semibold'>{item.company}</p>
+                  <p className='mt-2 text-xs md:text-base font-semibold italic'>Responsibilities:</p>
+                  <div dangerouslySetInnerHTML={{ __html: item.responsibilities }} className='content-list text-xs md:text-base' />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </section>
-      <section className='py-14 flex justify-between gap-6 border-b-[1px]'>
+      <section className='py-10 md:py-14 flex flex-col md:flex-row justify-between gap-6 border-b-[1px]'>
         <div>
-          <h3 className='mb-6 text-3xl text-center md:text-start font-semibold'>Technical Skills</h3>
+          <h3 className='mb-6 text-xl md:text-3xl text-center md:text-start font-semibold'>Technical Skills</h3>
           <ul className='flex flex-wrap gap-3'>
-            {technicalSkills.map(skill => (
+            {technicalSkills.map((skill: Skill) => (
               <li key={uuid()} className='px-5 py-2 border-none rounded-full bg-gray-100'>
                 {skill.title}
               </li>
@@ -55,9 +58,9 @@ const About = async () => {
           </ul>
         </div>
         <div>
-          <h3 className='mb-6 text-3xl text-center md:text-start font-semibold'>Additional Skills</h3>
+          <h3 className='mb-6 text-xl md:text-3xl text-center md:text-start font-semibold'>Additional Skills</h3>
           <ul className='flex flex-wrap gap-3'>
-            {additionalSkills.map(skill => (
+            {additionalSkills.map((skill: Skill) => (
               <li key={uuid()} className='px-5 py-2 border-none rounded-full bg-gray-100'>
                 {skill.title}
               </li>
@@ -65,26 +68,28 @@ const About = async () => {
           </ul>
         </div>
         <div>
-          <h3 className='mb-6 text-3xl text-center md:text-start font-semibold'>Education</h3>
-          <ul>
-            {userInfo.education.map(item => (
-              <li key={uuid()} className='mb-3 flex gap-x-10'>
-                <div className='text-base'>
-                  {`${formatDate(item.periodFrom, 'year')} - ${formatDate(item.periodTo, 'year')}`}
-                </div>
-                <div>
-                  <p className='text-lg font-semibold'>{item.institution}</p>
-                  <p className='font-semibold'>{item.degree}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <h3 className='mb-6 text-xl md:text-3xl text-center md:text-start font-semibold'>Education</h3>
+          <table>
+            <tbody>
+              {userInfo.education.map(item => (
+                <tr key={uuid()}>
+                  <td className='pr-6 h-full text-xs md:text-base  align-top'>
+                    {`${formatDate(item.periodFrom, 'year')}-${formatDate(item.periodTo, 'year')}`}
+                  </td>
+                  <td>
+                    <p className='text-sm md:text-lg font-semibold'>{item.institution}</p>
+                    <p className='text-xs md:text-base font-semibold'>{item.degree}</p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </section>
-      <section className='py-14 border-b-[1px]'>
-        <h3 className='mb-6 text-4xl text-center font-semibold'>Courses & Certifications</h3>
-        <p className='px-10 md:px-52 text-center'>From the first day when I first got into front-end development until now, I have completed plenty of courses and learning programs. I gained a solid understanding of web development standards and principles.</p>
-        <ul className='py-12 flex flex-wrap gap-6'>
+      <section className='py-10 md:py-14 border-b-[1px]'>
+        <h3 className='mb-6 text-2xl md:text-4xl text-center font-semibold'>Courses & Certifications</h3>
+        <p className='px-10 md:px-52 text-xs md:text-base text-center'>From the first day when I first got into front-end development until now, I have completed plenty of courses and learning programs. I gained a solid understanding of web development standards and principles.</p>
+        <ul className='py-6 md:py-12 flex flex-wrap gap-6'>
           {certifications.map(certificate => (
             <li key={uuid()} className='relative border border-gray-100 rounded-lg overflow-hidden'>
               <CertificateModal 
