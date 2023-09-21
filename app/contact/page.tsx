fb9324102054cf85710eec16f13681c02b47b72d
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-09-21 19:10:16 +0300

    fix contact page CTA button

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,13 +7,13 @@ export default function Contact() {
   return (
     <div className='mx-auto py-6 w-full h-full md:w-1/2 overflow-hidden bg-white'>
       <h2 className='mb-10 font-bold text-5xl text-center animate-from-top'>Let's talk!</h2>
-      <div>
-        <Link href='mailto:ivandaniliuk@gmail.com' className='flex justify-center items-center my-16 p-10 text-5xl border-2 rounded-[50%] bg-black text-white pulse'>
+      <div className='flex justify-center'>
+        <Link href='mailto:ivandaniliuk@gmail.com' className='flex justify-center items-center my-16 w-32 h-32 text-5xl border-2 rounded-full bg-black text-white pulse'>
           <FontAwesomeIcon icon={faEnvelope} />
         </Link>
       </div>
       <div className='bg-gray-100 line' />
-      <div>
+      <div className='px-10 md:px-32'>
         <SocialMediaList orientation='horizontal' />
       </div>
     </div>
