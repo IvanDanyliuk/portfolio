afca8eb24fa891661ca6b42762cb546012a2aba7
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-09-24 14:39:23 +0300

    add the mobile version of projects page filters

--- components/ui/ProjectFilters.tsx
+++ components/ui/ProjectFilters.tsx
@@ -3,10 +3,14 @@
 import { useEffect, useState } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import { Controller, useForm } from 'react-hook-form';
+import { Popover, Transition } from '@headlessui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { v4 as uuid } from 'uuid';
 import Button from './Button'
 import { Technology } from '@/common.types'
 import Checkbox from './Checkbox';
+import { useWindowSize } from '@/hooks/useWindowSize';
 
 interface ProjectFiltersProps {
   categories: string[];
@@ -21,6 +25,8 @@ interface FiltersForm {
 const ProjectFilters: React.FC<ProjectFiltersProps> = ({ categories, technologies }) => {
   const router = useRouter();
   const pathname = usePathname();
+
+  const { width } = useWindowSize();
 
   const [checkedCategories, setCheckedCategories] = useState<string[]>([]);
   const [checkedTechnologies, setCheckedTechnologies] = useState<string[]>([]);
@@ -79,8 +85,8 @@ const ProjectFilters: React.FC<ProjectFiltersProps> = ({ categories, technologie
     router.push(setSearchParams(pathname, data.categories, data.technologies));
   }, [data, reset]);
 
-  return (
-    <div className='min-h-full top-0'>
+  const filterForm = (
+    <>
       <h4 className='mb-6 text-xl font-semibold'>Filters</h4>
       <form onSubmit={handleSubmit((data) => console.log('SUBMIT', data))} className='flex flex-col gap-6'>
         <fieldset>
@@ -119,7 +125,45 @@ const ProjectFilters: React.FC<ProjectFiltersProps> = ({ categories, technologie
         </fieldset>
         <Button title='Clear Filters' onClick={clearFilters} />
       </form>
-    </div>
+    </>
+  );
+
+  return (
+    <>
+      {width! < 785 ? (
+        <div className='fixed bottom-0 right-0 z-50'>
+          <Popover className='relative flex items-end'>
+            {({ open }) => (
+              <>
+                <Popover.Button className='m-3 w-12 h-12 text-lg border border-black rounded-full bg-white z-50'>
+                  <FontAwesomeIcon icon={open ? faXmark : faFilter} />
+                </Popover.Button>
+                <Popover.Overlay className='fixed inset-0 bg-black opacity-50' />
+                <Transition
+                  show={open}
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Popover.Panel>
+                    <div className='w-auto h-screen px-10 py-5 bg-white'>
+                      {filterForm}
+                    </div>
+                  </Popover.Panel>
+                </Transition>
+              </>
+            )}
+          </Popover>
+        </div>
+      ) : (
+        <div className='min-h-full top-0'>
+          {filterForm}
+        </div>
+      )}
+    </>
   )
 }
 
