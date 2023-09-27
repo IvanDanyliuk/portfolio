dee89f4be60243589f634fbbc592e0146bb65d2c
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-09-27 16:30:52 +0300

    styles bug fix: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ import SocialMediaList from '@/components/ui/SocialMediaList';
 export default function Home() {
   return (
     <div className='flex w-full relative flex-col grow justify-between overflow-hidden'>
-      <div className='flex flex-col md:flex-row grow justify-between items-center'>
-        <div className='relative w-full md:py-0 md:h-auto greeting-block'>
+      <div className='h-full flex flex-col md:flex-row grow justify-center md:justify-between items-center'>
+        <div className='relative w-auto pb-10 md:pb-0 md:h-auto greeting-block'>
           <h1 className='mainHeading'>Hi! I am <strong className='font-bold'>Ivan Danyliuk</strong>.</h1>
           <h2 className='subHeading'>I am a creative frontend developer based in Ukraine</h2>
         </div>
-        <div className='relative w-full md:w-1/2 flex grow flex-col gap-5 items-center justify-center btns-block'>
+        <div className='relative w-full md:w-1/2 flex flex-col gap-5 items-center justify-center btns-block'>
           <Link 
             href='/projects' 
             className='cta-link'
@@ -29,7 +29,7 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div className='w-full md:w-1/6 h-20 px-7 md:px-0 flex items-center social-media'>
+      <div className='w-full md:w-1/6 h-20 px-7 md:px-0 flex justify-between items-center'>
         <SocialMediaList orientation='horizontal' />
       </div>
     </div>
