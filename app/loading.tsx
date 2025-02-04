f0d726f435721243b5379561f4fe80b7a10e3bcc
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-02-04 20:16:47 +0200

    remove files

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Loader from '@/components/ui/common/Loader'
-
-const LoadingPage: React.FC = () => {
-  return (
-    <Loader />
-  )
-}
-
-export default LoadingPage
--- lib/models/technology.model.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const technologylSchema = new mongoose.Schema({
-  title: { type: String, required: true }
-});
-
-const Technology = mongoose.models.Technology || mongoose.model('Technology', technologylSchema);
-
-export default Technology;
